bafcfe112d67ecd1b2e7c78e34be64c14e9604f4
aomzin <winittra_saengsroy@cmu.ac.th>
2021-11-26 18:26:41 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,35 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int x ;
+    int Even = 0 ; 
+    int Odd = 0 ;
+    int N = 0 ;
+   
+   
+    while ( x != N ){ 
+        cout << "Enter an integer: ";
+        cin >> x ; 
+       
+         if (x %2 == 0) {
+            Even++ ; //ถ้าพบข้อมูลเป็นเลขคู่ให้นับ +1ไปเรื่อยๆ
+                if ( x == 0 ) {
+                    Even-- ; //ยกเว้น 0 เพราะปกติ 0 เป็น Even ทำให้มัน--เป็น0ตามโจทย์
+            }
+ 
+        }else{
+            Odd++ ; //ถ้าพบข้อมูลเป็นเลขคี่ให้นับ +1ไปเรื่อยๆ
+        }
+
+    }
+        
+    cout << "#Even numbers = " << Even << "\n"; //รวมจำนวนคู่คี่
+    cout << "#Odd numbers = " << Odd ;
+  
     return 0;
 }
+    
+
+    
+  
